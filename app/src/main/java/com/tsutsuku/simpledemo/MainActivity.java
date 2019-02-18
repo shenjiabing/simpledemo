ed91b0d7560bb7ea92f64b0eb70afd2ab5dbde74
shen <1321058652@qq.com>
2019-02-18 15:45:20 +0800

    第5次提交  绑定控件

--- app/src/main/java/com/tsutsuku/simpledemo/MainActivity.java
+++ app/src/main/java/com/tsutsuku/simpledemo/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv_title=findViewById(R.id.tv_title);
+
     }
 }
