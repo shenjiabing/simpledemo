66014d3d8fed0a94d129ae8be0a8dd871b932cdd
shen <1321058652@qq.com>
2019-02-18 15:42:56 +0800

    第5次提交  绑定控件

--- app/src/main/java/com/tsutsuku/simpledemo/MainActivity.java
+++ app/src/main/java/com/tsutsuku/simpledemo/MainActivity.java
@@ -2,12 +2,18 @@ package com.tsutsuku.simpledemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private TextView tv_title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_title=findViewById(R.id.tv_title);
     }
 }
